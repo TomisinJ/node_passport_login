c5823bb13cf65022470273366da29a82de85c0c2
Tomisin <tomisinjoseph3@gmail.com>
2023-11-15 11:29:42 +0000

    Registered users

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require('express')
 const app = express()
+const bcrypt = require('bcrypt')
+
+const users = []
 
 app.set('view-engine', 'ejs')
+
+// take forms and access them inside the request variable inside the post method
+app.use(express.urlencoded({ extended:false }))
 
 app.get('/', (req, res) => {
     res.render('index.ejs', { name: "Tomi"})
@@ -20,8 +26,22 @@ app.get('/register', (req, res) => {
     res.render('register.ejs')
 })
 
-app.post('/register', (req, res) => {
-    
+// id is automatically generated in database, if we had one
+
+app.post('/register', async (req, res) => {
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10)
+        users.push({
+            id: Date.now().toString(),
+            name: req.body.name,
+            email: req.body.email,
+            password: hashedPassword
+        })
+        res.redirect('/login')
+    } catch {
+        res.redirect('/register')
+    }
+    console.log(users)
 })
 
 app.listen(3000)
